2dfffba6e6eb87558215bf259ff405cd13b2d27b
VINODvoid <iamlearner.v@gmail.com>
2024-09-20 17:22:25 +0530

    Pass by Reference

---
+++ Week-01/01basics.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+void doSomething(int &num)
+{
+    num = num + 10;
+    cout << " " << num << endl;
+}
+int main() {
+    int x = 1;
+    // int y = 3;
+
+    // switch (x) {
+    //     case 1:
+    //         cout << "x is 1." << endl;
+    //         switch (y) {
+    //             case 1:
+    //                 cout << "y is 1." << endl;
+    //                 break;
+    //             default:
+    //                 cout << "y is not 1." << endl;
+    //         }
+    //         break;
+    //     default:
+    //         cout << "x is not 1." << endl;
+    // }
+
+    // Pass by Reference
+    doSomething(x);
+    cout << x;
+    return 0;
+}
