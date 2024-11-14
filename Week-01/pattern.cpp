897c1fc1556e43b520691426f1d1fd6faccc3423
VINODvoid <iamlearner.v@gmail.com>
2024-11-14 07:23:41 +0530

    chore: add triangle patterns

--- Week-01/pattern.cpp
+++ Week-01/pattern.cpp
@@ -17,12 +17,6 @@ void pattern1(int n)
     }
     cout<<endl;
     }
-    // ? Output 
-// Enter the number of rows: 4
-// ****
-// ****
-// ****
-// ****
 }
 
 void pattern2(int n)
@@ -61,6 +55,58 @@ void pattern4(int n)
         cout<<endl;
     }
 }
+void pattern5(int n)
+{
+    cout<<"Pattern 5:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void pattern6(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern7(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        // space loop
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        // star loop
+        for(int j=0;j<2*i+1;j++)
+        {
+            cout<<"*";
+        }
+        // space loop
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern8(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        
+    }
+}
 int main(){
     int n;
     cout<<"Enter the number of rows: ";
@@ -69,6 +115,9 @@ int main(){
     pattern2(n);
     pattern3(n);
     pattern4(n);
-
+    pattern5(n);
+    pattern6(n);
+    pattern7(n);
+    pattern8(n);
     return 0;
 }
