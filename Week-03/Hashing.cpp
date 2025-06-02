46fb9f657d3885b44258784636eb19c5d31d8200
VINODvoid <iamlearner.v@gmail.com>
2025-06-02 05:56:42 +0530

    feat: add Hashing.cpp for counting occurrences of elements in an array

---
+++ Week-03/Hashing.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+void countNum(int n,int arr[]);
+int main()
+{
+    int n;
+    cout<<"Enter the size"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    int hash[13] ={0};
+    for(int i=0;i<n;i++)
+    {
+        hash[arr[i]] +=1;
+    }
+    cout<<"Enter the q numbers"<<endl;
+    int qnum;
+    cin>>qnum;
+    cout<<"Enter the q elements"<<endl;
+    while (qnum--)
+    {
+        int num ;
+        cin>>num;
+        cout<<"Value is "<<hash[num]<<endl;
+    }
+    
+
+    
+    return 0;
+}
+// First Approach
+// void countNum(int n,int arr[])
+// {
+//     int count =0;
+//     for (int i = 0; i <11; i++)
+//     {
+//         if(arr[i]==n)
+//         {
+//             count++;
+//         }
+//     }
+//     cout<<count;
+    
+// }
