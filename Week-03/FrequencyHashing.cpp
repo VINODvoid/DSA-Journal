db07a4ae01bcfb24a579ff50d49c1bfb8b0499b8
VINODvoid <iamlearner.v@gmail.com>
2025-06-02 14:51:11 +0530

    feat : highest frequency

--- Week-03/FrequencyHashing.cpp
+++ Week-03/FrequencyHashing.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
 #include<unordered_map>
+#include<map>
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin>>n;
+    int arr[n];
+    map<int ,int>hashmap;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        hashmap[arr[i]]++;
+    }
+
+    int maxFreq=0;
+    int maxEle;
+    for(auto it:hashmap)
+    {
+        
+        if(it.second >maxFreq )
+        {
+            maxFreq = it.second;
+            maxEle = it.first;
+        }
+    }
+    cout<<"Frequency of Element " << maxEle << " is "<<maxFreq<<endl;
+    cout<<endl;
     return 0;
 }
