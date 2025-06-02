7afbe194b6dab0b5a5e09659f8f17a024d7cd5ab
VINODvoid <iamlearner.v@gmail.com>
2025-06-02 07:00:13 +0530

    fix: improve formatting in Hashing.cpp for better readability

--- Week-03/Hashing.cpp
+++ Week-03/Hashing.cpp
@@ -1,36 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void countNum(int n,int arr[]);
+void countNum(int n, int arr[]);
 int main()
 {
     int n;
-    cout<<"Enter the size"<<endl;
-    cin>>n;
+    cout << "Enter the size" << endl;
+    cin >> n;
     int arr[n];
-    cout<<"Enter the elements"<<endl;
+    cout << "Enter the elements" << endl;
     for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    int hash[13] ={0};
-    for(int i=0;i<n;i++)
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++)
     {
-        hash[arr[i]] +=1;
-    } 
-    
-    cout<<"Enter the q numbers"<<endl;
+        hash[arr[i]] += 1;
+    }
+
+    cout << "Enter the q numbers" << endl;
     int qnum;
-    cin>>qnum;
-    cout<<"Enter the q elements"<<endl;
+    cin >> qnum;
+    cout << "Enter the q elements" << endl;
     while (qnum--)
     {
-        int num ;
-        cin>>num;
-        cout<<"Value is "<<hash[num]<<endl;
+        int num;
+        cin >> num;
+        cout << "Value is " << hash[num] << endl;
     }
-    
 
-    
     return 0;
 }
 // First Approach
@@ -45,5 +43,5 @@ int main()
 //         }
 //     }
 //     cout<<count;
-    
+
 // }
