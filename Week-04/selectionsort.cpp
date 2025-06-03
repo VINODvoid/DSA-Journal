d657197f9ea9f7339713114834b0cc188bcae492
VINODvoid <iamlearner.v@gmail.com>
2025-06-03 06:32:18 +0530

    feat: implement selection sort algorithm in selectionsort.cpp

---
+++ Week-04/selectionsort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void selectionsort(int arr[],int n)
+{
+    for (int i = 0; i <=n-2 ; i++)
+    {
+        int min = i;
+        for(int j=i;j<=n-1;j++)
+        {
+                if(arr[j]<=arr[min]) min = j;
+        }
+        swap(arr[i],arr[min]);
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i] <<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    selectionsort(arr,n);
+    return 0;
+}
