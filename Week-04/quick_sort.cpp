8dbd792c2a20bc928b2048ec0111e17f8c2f3264
VINODvoid <iamlearner.v@gmail.com>
2025-06-08 13:17:09 +0530

    feat: add initial implementation of quick sort algorithm in quick_sort.cpp

---
+++ Week-04/quick_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void quick_sort(int arr[],int n)
+{
+    
+
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    quick_sort(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
