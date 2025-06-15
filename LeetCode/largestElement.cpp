4cf387f325a37690568fdaf32545fe316ce9791c
VINODvoid <iamlearner.v@gmail.com>
2025-06-15 20:21:48 +0530

    feat: add program to find the largest element in an array

---
+++ LeetCode/largestElement.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[6] = {2,3,12,54,23,1276};
+    int largest = 0;
+    for (int i = 0; i <6 ; i++)
+    {
+        if(largest<arr[i]) largest = arr[i];
+    }
+    cout<<largest;
+    
+    return 0;
+}
