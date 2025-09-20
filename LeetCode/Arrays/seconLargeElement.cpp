6df023c07021278051da929668ddac05e434f185
VINODvoid <kalki.the.dev@gmail.com>
2025-09-20 07:40:32 +0530

    refactor: update C++ standards to C23 and C++23; improve code formatting and readability

--- LeetCode/Arrays/seconLargeElement.cpp
+++ LeetCode/Arrays/seconLargeElement.cpp
@@ -1,56 +1,34 @@
 #include<iostream>
 using namespace std;
-int second_largest(int arr[], int n)
+int s_largest(int arr[],int n)
 {
-    int largest = arr[0];
-    int s_largest = -1;
-    for(int i=1;i<n;i++)
-    {
-        if(arr[i]>largest)
-        {
-            s_largest = largest;
-            largest = arr[i];
-        }
-        if(arr[i]<largest && arr[i]>s_largest)
-        {
-            s_largest = arr[i];
-        }
-    }
+ int largest = arr[0];
+ int s_largest = -1;
+ for(int i=1;i<n;i++)
+ {
+    if(arr[i]>largest) largest = arr[i];
+
+    if(arr[i]<largest && arr[i]>s_largest) s_largest = arr[i];
+ }
     return s_largest;
 }
-int second_smallest(int arr[],int n)
+int s_smallest(int arr[],int n)
 {
     int smallest = arr[0];
-    int s_smallest = -1;
-    for (int i = 1; i < n; i++){
-        if(arr[i]<smallest)
-        {
-            s_smallest = smallest;
-            smallest = arr[i];
-        }
-        if(arr[i]>s_smallest && arr[i]<smallest)
-        {
-            s_smallest = arr[i];
-        }
-    }
-    
+    int s_smallest = arr[1];
 
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]<smallest) smallest = arr[i];
+        if(arr[i]>smallest && arr[i]<s_smallest) s_smallest = arr[i];
+    }
     return s_smallest;
 }
-int main()
-{
-    int n;
-    cout<<"Enter the value of n";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter the values";
-    for (int i = 0; i < n; i++)
-    {
-        cin>>arr[i];
-    }
-    int s_largest =second_largest(arr,n);
-    int s_smallest = second_smallest(arr,n);
-    cout<<"Second Largest number is" << s_largest<<"\nSecond Smallest number is "<<s_smallest<<endl;
-
+int main(){
+    int arr[6] = {2,5,313,56,35,90};
+    cout<<"The Second Largest Element is :\n";
+    cout<<s_largest(arr,6)<<endl;
+    cout<<"The Second Smallest Element is :\n";
+    cout<<s_smallest(arr,6)<<endl;
     return 0;
 }
--- LeetCode/Arrays/sortedArray.cpp
+++ LeetCode/Arrays/sortedArray.cpp
@@ -4,30 +4,19 @@ bool sorted_array(int arr[],int n)
 {
     for(int i=0;i<n;i++)
     {
-        if(arr[i]>arr[i+1])
-        {
-            return false;
-        }
+        if(arr[i]>arr[i+1]) return false;
     }
     return true;
 }
 int main()
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
+    int arr[6] = {1,2,3,5,6,4};
+    if(sorted_array(arr,6))
     {
-        cin>>arr[i];
-    }
-    bool isSorted = sorted_array(arr,n);
-    if(isSorted == 1)
-    {
-        cout<<"Sorted";
+        cout<<"Given array is sorted\n"<<endl;
     }
     else{
-        cout<<"Not sorted"<<endl;
+        cout<<"Given array is not sorted\n";
     }
-    
     return 0;
 }
